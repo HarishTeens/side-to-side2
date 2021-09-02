f23640fcab896c27bb13fbb6bd8810f3225a8502
Harish <33366456+HarishTeens@users.noreply.github.com>
2021-09-02 05:59:16 +0000

    show score

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // DOM elements
-const character = document.getElementById("character");
-const block = document.getElementById("block");
+const elements={
+	character:document.getElementById("character"),
+	block:document.getElementById("block"),
+	score: document.getElementById("score")
+}
 
 // Game Vars
 let score = 0;
@@ -14,37 +17,41 @@ document.addEventListener("keydown", event => {
 document.getElementById("right").addEventListener("touchstart", moveRight);
 document.getElementById("left").addEventListener("touchstart", moveLeft);
 
-
-function moveLeft(){
-    let left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    left -= 100;
-    if(left>=0){
-        character.style.left = left + "px";
-    }
-}
-function moveRight(){
-    let left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    left += 100;
-    if(left<300){
-        character.style.left = left + "px";
-    }
-}
-
-block.addEventListener('animationiteration', () => {
-    var random = Math.floor(Math.random() * 3);
+elements.block.addEventListener('animationiteration', () => {
+    let random = Math.floor(Math.random() * 3);
     left = random * 100;
-    block.style.left = left + "px";
-    score++;
+    elements.block.style.left = left + "px";
+    updateScore();
 });
 
 // Core game logic to check hit
 setInterval(function(){
-    var characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    var blockTop = parseInt(window.getComputedStyle(block).getPropertyValue("top"));
+    const characterLeft = parseInt(window.getComputedStyle(elements.character).getPropertyValue("left"));
+    const blockLeft = parseInt(window.getComputedStyle(elements.block).getPropertyValue("left"));
+    const blockTop = parseInt(window.getComputedStyle(elements.block).getPropertyValue("top"));
     if(characterLeft==blockLeft && blockTop<500 && blockTop>300){
         alert("Game over. Score: " + score);
-        block.style.animation = "none";
+        elements.block.style.animation = "none";
+		// make a reset game function to do all the reset operations there
     }
 },1);
 
+// GAME FUNCTIONS
+function moveLeft(){
+    let left = parseInt(window.getComputedStyle(elements.character).getPropertyValue("left"));
+    left -= 100;
+    if(left>=0){
+        elements.character.style.left = left + "px";
+    }
+}
+function moveRight(){
+    let left = parseInt(window.getComputedStyle(elements.character).getPropertyValue("left"));
+    left += 100;
+    if(left<300){
+        elements.character.style.left = left + "px";
+    }
+}
+function updateScore(){
+	score=score+1;
+	elements.score.innerText=score;
+}
