234baf44123bf1c88906034ceb213a1b111ec204
Harish <33366456+HarishTeens@users.noreply.github.com>
2021-09-02 07:19:13 +0000

    level 1

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
+// Game Vars
+let score = 0;
+let highScore;
+
 // DOM elements
 const elements = {
 	character: document.getElementById("character"),
 	block: document.getElementById("block"),
 	score: document.getElementById("score"),
-	level: document.getElementById("level")
+	level: document.getElementById("level"),
+	highScore: document.getElementById("highscore")
 }
 
-// Game Vars
-let score = 0;
-if (!localStorage.getItem("highScore")) {
-	localStorage.setItem("highScore", 0)
-  elements.highscore.innerText=localStorage.getItem("highScore")
+// Game Setup
+function gameSetup(){
+	if (!localStorage.getItem("highScore")) {
+		localStorage.setItem("highScore", 0);
+	}
+	highScore=parseInt(localStorage.getItem("highScore"));
+	elements.highScore.innerText=highScore;
 }
+gameSetup();
 
 // MOVEMENT
 document.addEventListener("keydown", event => {
 	if (event.key === "ArrowLeft") { moveLeft(); }
 	if (event.key === "ArrowRight") { moveRight(); }
 });
+
 // Touch action for mobile instead of arrow keys
 document.getElementById("right").addEventListener("touchstart", moveRight);
 document.getElementById("left").addEventListener("touchstart", moveLeft);
@@ -30,35 +39,35 @@ elements.block.addEventListener('animationiteration', () => {
 
 // Core game logic to check hit
 setInterval(function () {
-	var characterLeft = parseInt(window.getComputedStyle(elements.character).getPropertyValue("left"));
-	var blockLeft = parseInt(window.getComputedStyle(elements.block).getPropertyValue("left"));
-	var blockTop = parseInt(window.getComputedStyle(elements.block).getPropertyValue("top"));
+	const characterLeft = parseInt(window.getComputedStyle(elements.character).getPropertyValue("left"));
+	const blockLeft = parseInt(window.getComputedStyle(elements.block).getPropertyValue("left"));
+	const blockTop = parseInt(window.getComputedStyle(elements.block).getPropertyValue("top"));
 	if (characterLeft == blockLeft && blockTop < 500 && blockTop > 300) {
-		alert("Game over. Score: " + score);
-		elements.block.style.animation = "none";
-    if(score>localStorage.getItem("highScore"))
-      { 
-        localStorage.setItem("highScore",score)
-      }
-      elements.highscore.innerText=localStorage.getItem("highScore")
-		// make a reset game function to do all the reset operations there
+		gameOver();
 	}
 }, 1);
 
 // GAME FUNCTIONS
-function moveLeft() {
+function moveLeft() {  
 	let left = parseInt(window.getComputedStyle(elements.character).getPropertyValue("left"));
 	left -= 300;
 	if (left >= 0) {
 		elements.character.style.left = left + "px";
 	}
 }
-function moveRight() {
+function moveRight() {  
 	let left = parseInt(window.getComputedStyle(elements.character).getPropertyValue("left"));
 	left += 300;
 	if (left < 900) {
 		elements.character.style.left = left + "px";
 	}
+}
+function gameOver(){
+	alert("Game over. Score: " + score);
+	elements.block.style.animation = "none";
+	if(score>parseInt(localStorage.getItem("highScore")))
+		localStorage.setItem("highScore",score)
+	elements.highScore.innerText=localStorage.getItem("highScore")
 }
 
 function deployObstacle() {
