a71393a06c2866f3cb77b4870a5cee469cbb9e8f
Harish <33366456+HarishTeens@users.noreply.github.com>
2021-09-02 05:51:36 +0000

    refactor

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+// DOM elements
+const character = document.getElementById("character");
+const block = document.getElementById("block");
+
+// Game Vars
+let score = 0;
+
+// MOVEMENT
 document.addEventListener("keydown", event => {
   if(event.key==="ArrowLeft"){moveLeft();}
   if(event.key==="ArrowRight"){moveRight();}
 });
-var character = document.getElementById("character");
+// Touch action for mobile instead of arrow keys
+document.getElementById("right").addEventListener("touchstart", moveRight);
+document.getElementById("left").addEventListener("touchstart", moveLeft);
+
+
 function moveLeft(){
     let left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
     left -= 100;
@@ -17,26 +29,22 @@ function moveRight(){
         character.style.left = left + "px";
     }
 }
-var block = document.getElementById("block");
-var counter = 0;
+
 block.addEventListener('animationiteration', () => {
     var random = Math.floor(Math.random() * 3);
     left = random * 100;
     block.style.left = left + "px";
-    counter++;
+    score++;
 });
+
+// Core game logic to check hit
 setInterval(function(){
     var characterLeft = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     var blockTop = parseInt(window.getComputedStyle(block).getPropertyValue("top"));
     if(characterLeft==blockLeft && blockTop<500 && blockTop>300){
-        alert("Game over. Score: " + counter);
+        alert("Game over. Score: " + score);
         block.style.animation = "none";
     }
 },1);
 
-
-
-
-document.getElementById("right").addEventListener("touchstart", moveRight);
-document.getElementById("left").addEventListener("touchstart", moveLeft);
